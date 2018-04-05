6658aa2391a07488e80a2511b8cdf11803f6cee7
Couper12 <coupersemail@gmail.com>
2018-04-05 13:18:32 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,20 +27,22 @@ namespace u2_spaceship_Couper
     public partial class MainWindow : Window
     {
         
-        //delcaring a point variable
+        //delcaring point variables
         Point location1;
         Point location2;
 
+        // delcaring spaceship variables
         Spaceship spaceship1;
         Spaceship spaceship2;
 
         public MainWindow()
         {
             InitializeComponent();
-            
+             
         }
 
-        private void btn2Players_Click(object sender, RoutedEventArgs e)
+        // pressing the button will build two spaceships on either  side of the canvas
+        private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             location1 = new Point(50, 342);
             location2 = new Point(550, 349);
@@ -48,8 +50,10 @@ namespace u2_spaceship_Couper
             spaceship2 = new Spaceship(location2, canvas, Brushes.Blue);
         }
 
+        // the following stuff will happen when a key is pressed
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            // when these keys are pressed, spaceship 1 will turn and move
             int orientation1 = spaceship1.turn(e.Key);
             if (e.Key == Key.Left
                 || e.Key == Key.Up
@@ -58,6 +62,7 @@ namespace u2_spaceship_Couper
             {
                 location1 = spaceship1.fly(orientation1, spaceship1, location1);
             }
+            // when these keys are pressed spaceship 2 wll turn and move
             if (e.Key == Key.A
                 || e.Key == Key.W
                 || e.Key == Key.S
@@ -66,10 +71,13 @@ namespace u2_spaceship_Couper
                 location2 = spaceship2.fly(orientation1, spaceship2, location2);
             }
 
+            // the spaceship location will change
             Canvas.SetLeft(spaceship1.rect, location1.X);
             Canvas.SetTop(spaceship1.rect, location1.Y);
             Canvas.SetLeft(spaceship2.rect, location2.X);
             Canvas.SetTop(spaceship2.rect, location2.Y);
+
+            // check if the spaceships have crashed
             spaceship1.checkCollision(location1, location2);
         }
     }
